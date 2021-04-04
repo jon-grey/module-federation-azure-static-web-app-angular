68367c1771ca15b737db25c4a09789d0b27ee358
Jon Grey <jon.arnold.grey@gmail.com>
2021-04-05 01:22:52 +0200

    Make local libs as dependencies in package.json - build with apps.

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -7,8 +7,6 @@ import {loadRemoteEntry } from "@angular-architects/module-federation";
 
 loadRemoteEntry('http://localhost:3000/remoteEntry.js', 'mfe1')
 .catch(err => console.error(err))
-loadRemoteEntry('https://lively-flower-04e21dc10.azurestaticapps.net/remoteEntry.js', 'mfe1')
-.catch(err => console.error(err))
 loadRemoteEntry('https://salmon-moss-0f41c3910.azurestaticapps.net/remoteEntry.js', 'mfe1')
 .catch(err => console.error(err))
 .then(_ => import('./bootstrap'))
